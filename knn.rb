999e36da7d85b2d77197bbf04fb6189c9cc2e940
Adham EL-Deeb <adham_eldeeb90@hotmail.com>
2014-08-27 09:03:17 +0300

    EDIT:knn with > euclidean_distance && ADD: knn_test.rb

--- knn.rb
+++ knn.rb
@@ -1,6 +1,10 @@
+#!/usr/bin/env ruby
+
+
+
 class Knn
 	def initialize()
-	  @data = []
+	  @data           = []
 	  @id_to_position = {}
 	  @position_to_id = {}
 	end
@@ -21,7 +25,7 @@ class Knn
 		nearest_neighbors = []
 
 		@data.each_with_index do |instance, position|
-			distance = calculate_distance(instance, unkown_instance)
+			distance = euclidean_distance(instance, unkown_instance)
 			next if instance == unkown_instance
 
 			if nearest_neighbors.length < k
@@ -31,7 +35,7 @@ class Knn
 				max_distance = nearest_neighbors.last.first
 				if distance < max_distance
 					nearest_neighbors[k - 1] = [distance, position]
-					nearest_neighbors = sort_neighbors(nearest_neighbors)
+					nearest_neighbors        = sort_neighbors(nearest_neighbors)
 				end
 			end
 		end
@@ -43,7 +47,49 @@ class Knn
 	end
 	
 	def sort_neighbors(nearest_neighbors)
-		sort_neighbors.sort { |a, b| a[0] <=> b[0] }
+		nearest_neighbors.sort { |a, b| a[0] <=> b[0] }
+	end
+
+	def zip_sparse_arrays(instance1, instance2, &block)
+		position1 = 0
+		position2 = 0
+
+		current1  = instance1[position1]
+		current2  = instance2[position2]
+
+		while (position1 < instance1.length || position2 < instance2.length)
+			column1, value1 = current1
+			column2, value2 = current2
+			
+			column1 ||= column2
+			column2 ||= column1
+			value1  ||= value2
+			value2  ||= value1
+
+			if column1 == column2
+				block.call(value1, value2)
+				position1 += 1
+				position2 += 1
+				current1   = instance1[position1]
+				current2   = instance2[position2]
+			elsif column1 < column2
+				block.call(value1, 0)
+				position1 += 1
+				current1   = instance1[position1]
+			else
+				block.call(0, value2)
+				position2 += 1
+				current2   = instance2[position2]
+			end
+		end
+	end
+
+	def euclidean_distance(instance1, instance2)
+		total_distance = 0.0
+		zip_sparse_arrays(instance1, instance2) do |value1, value2|
+			total_distance += (value1 - value2) ** 2
+		end
+		Math.sqrt(total_distance)
 	end
 end 
 
---
+++ knn_test.rb
@@ -0,0 +1,16 @@
+require "#{File.dirname(__FILE__)}/knn.rb"
+
+knn = Knn.new
+
+data1 = [[0, 1],[1, 0]]
+data2 = [[0, 0],[1, 0]]
+data3 = [[0, 4],[1, 5]]
+data4 = [[0, 6],[1, 6]]
+
+
+knn.add_instance(data1, "1, 0")
+knn.add_instance(data2, "0, 0")
+knn.add_instance(data3, "4, 5")
+knn.add_instance(data4, "6, 6")
+
+puts knn.find_nearest_neighbors(data1, 3).inspect
